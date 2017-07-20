17bc0acff4dcb62e48f9ebc62f2a1d9bf61e93ed
harmeko <pierre.lethiec@epitech.eu>
2017-07-20 13:41:31 +0200

    fixed bugs, added members list

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const bot = new Discord.Client();
 
 var games = [];
 var comp  = null;
+// var LFGchannel = null;
+// https://discordapp.com/oauth2/authorize?client_id=337172231375421440&scope=bot&permissions=3072
 
 function game( game, name, time, timeStamp ) {
     return {
@@ -16,7 +18,8 @@ function game( game, name, time, timeStamp ) {
         timeStamp: timeStamp,
         timeSpan : time,
         creator  : name,
-        gameNo   : game
+        gameNo   : game,
+        members  : []
     }
 
 }
@@ -26,78 +29,210 @@ function getCountDown( game ) {
     var newDateObj = new Date();
     newDateObj.setTime( game.timeStamp.getTime() + (game.timeSpan * 60 * 1000) );
 
-    if ( newDateObj <= game.timeStamp )
-    {
-        console.log( "delete" );
-        delete games[games.indexOf( game )];
-        return false;
-    }
-    console.log( newDateObj <= game.timeStamp );
     return Countdown( null, newDateObj );
 }
 
-function findFunc( element ) {
-    return element.gameNo == comp;
-}
+// function findFunc( element ) {
+//     return element.gameNo == comp;
+// }
 
-var reg = new RegExp( '^!lfg.*' );
+var reg    = new RegExp( '^!lfg.*' );
+var regNum = new RegExp( '[0-9]{6}' );
+
+bot.on("ready", function () {
+    bot.user.setGame('Black Survival');
+});
 
 bot.on( "message", function ( msg ) {
-    if ( reg.test( msg.content ) )
-    {
-        var request = msg.content.split( ' ' );
-
-        if ( request[1] !== undefined )
+        if ( reg.test( msg.content ) )
         {
-            if ( request[1] === "add" )
-            {
-                if ( request[2] !== undefined || !isNaN( request[2] ) )
-                {
-                    var time = request[3] || 5;
-                    games.push( game( request[2], msg.author.username, time, new Date() ) );
-                    var count = getCountDown( games[games.length - 1] );
+            var request = msg.content.split( ' ' );
 
-                    if ( count )
+            if ( request[1] !== undefined )
+            {
+                if ( request[1] === "join" )
+                {
+                    // if ( request[2] !== undefined && regNum.test( request[2] ) )
+                    // {
+                    //     comp          = request[2];
+                    //     var instance2 = games.find( findFunc );
+                    var instance2 = games[0];
+                    if ( instance2 != undefined )
                     {
-                        msg.channel.send( "```css\n!----- Private game created -----!```\n" +
-                            "```http\n HOST : " + games[games.length - 1].creator +
-                            "\n Slots available : " + games[games.length - 1].slots +
-                            "\n Game code : " + games[games.length - 1].gameNo +
-                            "\n Time left : " + count.toString() + "```" );
+                        if ( instance2.members.indexOf( msg.author.username ) != -1 )
+                        {
+                            return msg.channel.send( "```You already are in the game.```" )
+                        }
+
+                        games[0].members.push( msg.author.username );
+
+                        var count4 = getCountDown( instance2 );
+                        if ( instance2.slots > 0 )
+                        {
+                            instance2.slots = instance2.slots - 1;
+                        }
+
+                        if ( instance2.slots == 0 )
+                        {
+                            instance2.slots = instance2.slots - 1;
+                        }
+                        return msg.channel.send( "```http\n HOST : " + instance2.creator +
+                            "\n Game code : " + instance2.gameNo +
+                            "\n Slots available : " + instance2.slots +
+                            "\n Time left : " + count4.toString() + "```" );
+                    }
+                    else
+                    {
+                        return msg.channel.send( "```Game not found```" );
+                    }
+                    // }
+                    // else
+                    // {
+                    //     return msg.channel.send( "```Game code is incorrect```" )
+                    // }
+                }
+
+                if ( request[1] === "leave" )
+                {
+
+                    // if ( request[2] !== undefined && regNum.test( request[2] ) )
+                    // {
+                    //     comp          = request[2];
+                    // var instance2 = games.find( findFunc );
+                    var instance2 = games[0];
+                    if ( instance2 != undefined )
+                    {
+                        if ( instance2.members.indexOf( msg.author.username ) == -1 )
+                        {
+                            return msg.channel.send( "```You are not in the game.```" )
+                        }
+
+                        if ( msg.author.username == instance2.creator )
+                        {
+                            return msg.channel.send( "```The creator can't leave a game, DESTROY the game instead.```" )
+                        }
+
+                        games[0].members.splice( games[0].members.indexOf( msg.author.username ), 1 );
+
+                        var count4 = getCountDown( instance2 );
+                        if ( instance2.slots < 9 )
+                        {
+                            instance2.slots = instance2.slots + 1;
+                        }
+                        return msg.channel.send( "```http\n HOST : " + instance2.creator +
+                            "\n Game code : " + instance2.gameNo +
+                            "\n Slots available : " + instance2.slots +
+                            "\n Time left : " + count4.toString() + "```" );
+                    }
+                    else
+                    {
+                        return msg.channel.send( "```Game not found```" );
+                    }
+                    // }
+                    // else
+                    // {
+                    //     return msg.channel.send( "```Game code is incorrect```" )
+                    // }
+                }
+
+                if ( request[1] === "DESTROY" )
+                {
+                    if ( msg.author.username === games[0].creator )
+                    {
+                        games = [];
+                        return msg.channel.send( "Yes master. \n ```Game destroyed```" )
+                    }
+                    else
+                    {
+                        return msg.channel.send( "I don't think so." )
                     }
                 }
-                else
-                {
-                    msg.channel.send( "Game code is incorrect" );
-                }
-            }
 
-            if ( request[1] === "info" )
-            {
-                if ( request[2] !== undefined )
+                if ( request[1] === "help" )
                 {
-                    if ( isNaN( request[2] ) )
+                    return msg.channel.send(
+                        "Parameters with () are mandatory, parameters with [] are optional but don't put () or [] in your message ;) \n" +
+                        "`!lfg add (game code) [minutes you will wait before starting]` where minutes have to be a number like `!lfg add 000000` for the default 5 minutes or `!lfg add 000000 6` for 6 minutes \n" +
+                        "`!lfg info` for informations about the current game \n" +
+                        "`!lfg join` to indicate you're joining the game` \n" +
+                        "`!lfg leave` to indicate you're leaving the game \n" +
+                        "`!lfg DESTROY` destroys the current game if you are the creator. It's in capital letters. It's not a mistake, it's your ALMIGHTY POWER AS A CREATOR! \n" +
+                        " © Harmeko" );
+                }
+
+                if ( request[1] === "add" )
+                {
+                    if ( request[2] !== undefined && regNum.test( request[2] ) )
+                    {
+                        if ( games.length >= 1 )
+                        {
+                            return msg.channel.send( "another game is already starting" );
+                        }
+                        var time = 5;
+                        if ( request[3] !== undefined && !isNaN( request[3] ) )
+                        {
+                            time = request[3];
+                        }
+
+                        if ( time >= 60 )
+                        {
+                            return msg.channel.send( "```You really think anyone's gonna wait this long? Niva, stop!```" )
+                        }
+
+                        games.push( game( request[2], msg.author.username, time, new Date() ) );
+                        var index = games.length - 1;
+                        var count = getCountDown( games[index] );
+                        games[0].members.push( msg.author.username );
+
+                        setInterval( function () {
+                            games.splice( 0, 1 );
+                        }, time * 60 * 1000 );
+
+                        msg.guild.channels.find( "name", "test-one" ).send(
+                            "```css\n!----- Private game created -----!```\n" +
+                            "```http\n HOST : " + games[index].creator +
+                            "\n Slots available : " + games[index].slots +
+                            "\n Game code : " + games[index].gameNo +
+                            "\n Time left : " + count.toString() + "``` \n" +
+                            "@LFG " + games[index].gameNo );
+
+                        return msg.channel.send( "```css\n!----- Private game created -----!```\n" +
+                            "```http\n HOST : " + games[index].creator +
+                            "\n Slots available : " + games[index].slots +
+                            "\n Game code : " + games[index].gameNo +
+                            "\n Time left : " + count.toString() + "```" );
+                    }
+                    else
                     {
                         return msg.channel.send( "Game code is incorrect" );
                     }
+                }
 
-                    comp         = request[2];
-                    var instance = games.find( findFunc );
+                if ( request[1] === "info" )
+                {
+                    // if ( request[2] !== undefined )
+                    // {
+                    // if ( !regNum.test( request[2] ) )
+                    // {
+                    //     return return msg.channel.send( "Game code is incorrect" );
+                    // }
+
+                    // comp         = request[2];
+                    // var instance = games.find( findFunc );
+                    var instance = games[0];
                     if ( instance != undefined )
                     {
                         var count2 = getCountDown( instance );
 
-                        if ( count2 )
-                        {
-                            msg.channel.send( "```http\n HOST : " + instance.creator +
-                                "\n Game code : " + instance.gameNo +
-                                "\n Slots available : " + instance.slots +
-                                "\n Time left : " + count2.toString() + "```" );
-                        }
+                        return msg.channel.send( "```http\n HOST : " + instance.creator +
+                            "\n Game code : " + instance.gameNo +
+                            "\n Slots available : " + instance.slots +
+                            "\n Time left : " + count2.toString() +
+                            "\n Members : " + instance.members.join( " , " ) + "```" );
                     }
                     else
                     {
-                        msg.channel.send( "```No games found```" );
+                        return msg.channel.send( "```No games found```" );
                     }
                 }
                 else
@@ -105,54 +240,21 @@ bot.on( "message", function ( msg ) {
                     games.forEach( function ( instance ) {
                         var count2 = getCountDown( instance );
 
-                        if ( count2 )
-                        {
-                            msg.channel.send( "```http\n HOST : " + instance.creator +
-                                "\n Game code : " + instance.gameNo +
-                                "\n Slots available : " + instance.slots +
-                                "\n Time left : " + count2.toString() + "```" );
-                        }
+                        return msg.channel.send( "```http\n HOST : " + instance.creator +
+                            "\n Game code : " + instance.gameNo +
+                            "\n Slots available : " + instance.slots +
+                            "\n Time left : " + count2.toString() + "```" );
                     } )
                 }
 
-                if ( request[1] === "join" )
-                {
-                    console.log("JOIN HERE");
-                    if ( request[2] !== undefined || !isNaN( request[2] ) )
-                    {
-                        comp          = request[2];
-                        var instance2 = games.find( findFunc );
-                        console.log("JOIN", instance2);
-                        if ( instance2 != undefined )
-                        {
-                            var count4 = getCountDown( instance2 );
-                            console.log("JOIN", count);
-                            if ( count4 )
-                            {
-                                instance2.slots = instance2.slots - 1;
-                                msg.channel.send( "```http\n HOST : " + instance2.creator +
-                                    "\n Game code : " + instance2.gameNo +
-                                    "\n Slots available : " + instance2.slots +
-                                    "\n Time left : " + count4.toString() + "```" );
-                            }
-                        }
-                        else
-                        {
-                            msg.channel.send( "```Game not found```" );
-                        }
-                    }
-                    else
-                    {
-                        msg.channel.send( "```Game code is incorrect```" )
-                    }
-                }
+            }
+            else
+            {
+                return msg.channel.send( "Request is empty, try `!lfg help` if needed " );
             }
         }
-        else
-        {
-            msg.channel.send( "Request is empty, try `!lfg help` if needed " );
-        }
     }
-} );
+)
+;
 
 bot.login( "MzM3MTcyMjMxMzc1NDIxNDQw.DFDAgQ.GFatl8TZz-NUvjP1F7L1pjqPDEQ" );
